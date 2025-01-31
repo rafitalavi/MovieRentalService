f93691faeffb2465c8490098195b3cf0c5f33832
rafitalavi <rafitalavi12345@gmail.com>
2025-01-31 23:43:32 +0600

    PopulateMembershipTypes

---
+++ Migrations/20250131170241_PopulateMembershipTypes.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieWala.Migrations
+{
+    public partial class PopulateMembershipTypes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Insert data into MembershipTypes table without specifying the Id
+            migrationBuilder.Sql("INSERT INTO MembershipTypes (SignUpFee, DurationInMonths, DiscountRate) VALUES (0, 0, 0)");
+            migrationBuilder.Sql("INSERT INTO MembershipTypes (SignUpFee, DurationInMonths, DiscountRate) VALUES (30, 1, 10)");
+            migrationBuilder.Sql("INSERT INTO MembershipTypes (SignUpFee, DurationInMonths, DiscountRate) VALUES (90, 3, 15)");
+            migrationBuilder.Sql("INSERT INTO MembershipTypes (SignUpFee, DurationInMonths, DiscountRate) VALUES (300, 12, 20)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // Remove the inserted data if the migration is rolled back
+            migrationBuilder.Sql("DELETE FROM MembershipTypes WHERE Id IN (1, 2, 3, 4)");
+        }
+    }
+}
